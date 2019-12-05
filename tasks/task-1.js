b60f028387c52e64e7abffee7520769a7b6cb916
MagicCurlyHair <g.shokanov@gmail.com>
2019-12-05 10:07:15 +0300

    add task 1.1 implementation

---
+++ tasks/task-1.js
@@ -0,0 +1,50 @@
+const { Buffer } = require('buffer');
+const stream = require('stream');
+const { EOL } = require('os');
+
+class ReverseTransform extends stream.Transform {
+    constructor(options) {
+        super(options);
+    }
+
+    static EOLBuffer = Buffer.from(EOL, 'utf8');
+
+    endsWithEOL(buffer) {
+        const { EOLBuffer } = ReverseTransform;
+        const possiblyEOL = buffer.slice(buffer.length - EOLBuffer.length, buffer.length);
+        return possiblyEOL.equals(EOLBuffer);
+    }
+
+    reverseBuffer(buffer, start, end) {
+        let i = start || 0;
+        let j = end - 1 || buffer.length - 1;
+        while (i < j) {
+            const tmp = buffer[i];
+            buffer[i] = buffer[j];
+            buffer[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    _transform(data, encoding, callback) {
+        this.endsWithEOL
+            ? this.reverseBuffer(data, 0, data.length - ReverseTransform.EOLBuffer.length)
+            : this.reverseBuffer(data);
+        this.push(data, encoding);
+        callback();
+    }
+}
+
+const reverse = new ReverseTransform();
+
+stream.pipeline(
+    process.stdin,
+    reverse,
+    process.stdout,
+    err => { 
+        if (err) {
+            console.error(err);
+        }
+    }
+);
