def862e0f35e2091339179b9e65f9a7ff5c24b34
MagicCurlyHair <g.shokanov@gmail.com>
2020-02-22 10:54:40 +0300

    change user id to be uuid

--- homework-2/src/models/user.model.ts
+++ homework-2/src/models/user.model.ts
@@ -1,19 +1,35 @@
 import { Model, Table, Column, DataType } from 'sequelize-typescript';
+import * as uuid from 'uuid/v4';
 
 @Table({
     modelName: 'user',
     timestamps: false
 })
 export class UserModel extends Model<UserModel> {
-    @Column
+    @Column({
+        type: DataType.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: uuid
+    })
+    id!: string;
+
+    @Column({
+        type: DataType.STRING(50),
+    })
     login!: string;
 
-    @Column
+    @Column({
+        type: DataType.STRING(50)
+    })
     password!: string;
 
     @Column(DataType.INTEGER)
     age!: number;
 
-    @Column
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: false
+    })
     isDeleted!: boolean;
 }
